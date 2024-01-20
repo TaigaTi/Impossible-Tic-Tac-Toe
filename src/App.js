bb3dace8d8795f58e06517aafb6fbd9040ee3db6
TaigaTi <tarikabirch@gmail.com>
2024-01-20 10:45:45 -0400

    added game logic

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import { useState } from "react";
 function Square({ value, onSquareClick }) {
   return (
     <>
-      <button className="square" onClick={onSquareClick}>{value}</button>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
     </>
   );
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  // Print Game Status
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  // Handle Square Click
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+    {/* Status */}
+    <div className="status">{status}</div>
+
+    {/* Board */}
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -36,4 +63,28 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+// Helper function to calculate winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2 ],
+    [3, 4, 5 ],
+    [6, 7, 8 ],
+    [0, 3, 6 ],
+    [1, 4, 7 ],
+    [2, 5, 8 ],
+    [0, 4, 8 ],
+    [2, 4, 6 ]
+  ];
+
+  for (let i = 0 ; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
